a81d0f0866ced2bc83f4a144a2188681e03e329b
Luke Karrys <lukekarrys@gmail.com>
2020-12-14 16:41:56 -0700

    13

---
+++ 13/index.js
@@ -0,0 +1,58 @@
+const {
+  lcm,
+  lastItem,
+  range,
+  primeFactors,
+  nextPrime,
+  mult,
+} = require("../util")
+
+const parseLines = (lines) => {
+  const [ts, trains] = lines
+  return {
+    ts: +ts,
+    trains: trains
+      .split(",")
+      .map((t, i) => (t === "x" ? t : [+t, i]))
+      .filter((x) => x !== "x"),
+  }
+}
+
+const problem1 = (lines) => {
+  const { ts, trains } = parseLines(lines)
+
+  const trainDeparturesNearTs = trains.map(([t]) => Math.ceil(ts / t) * t)
+
+  const trainMin = Math.min(...trainDeparturesNearTs)
+
+  const trainId =
+    trains[trainDeparturesNearTs.findIndex((t) => t === trainMin)][0]
+
+  return (trainMin - ts) * trainId
+}
+
+// https://www.reddit.com/r/adventofcode/comments/kcb3bb/2020_day_13_part_2_can_anyone_tell_my_why_this/
+// https://starboard.gg/robsh/advent-of-code-2020-n5gzhjY
+// Another day getting stuck in a way that felt exactly like Day 10
+// I came up with my own solution that was trying to do pretty much the same thing
+// but I couldn't get the right number start, step to pass to subsequent passes
+const findBus = (trains, start = 1, step = 1) => {
+  if (trains.length === 0) return start
+
+  const [train, offset] = trains.shift()
+  const multiple = step * train
+
+  for (let time = start; time <= multiple; time += step) {
+    if ((time + offset) % train === 0) {
+      return findBus(trains, time, multiple)
+    }
+  }
+
+  throw new Error("This should never reach")
+}
+
+const problem2 = (lines) => {
+  return findBus(parseLines(lines).trains)
+}
+
+module.exports = [problem1, problem2]
